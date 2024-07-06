dcc26977a6efa692de0cad3f827d6e15e5e02ccb
Ian Scott <scottianw@gmail.com>
2024-07-06 14:52:40 -0400

    Fixed syntax in new "non-blank" title field validation requirement
    (cherry picked from commit e90adb416e9e018a4c808b0e45a05bfc5da8d140)

--- invenio_rdm_records/services/schemas/metadata.py
+++ invenio_rdm_records/services/schemas/metadata.py
@@ -364,7 +364,7 @@ class MetadataSchema(Schema):
     )
     title = SanitizedUnicode(
         required=True, 
-        validate=validate._not_blank(_("Title cannot be a blank string."))
+        validate=_not_blank(_("Title cannot be a blank string."))
     )
     additional_titles = fields.List(fields.Nested(TitleSchema))
     publisher = SanitizedUnicode()
